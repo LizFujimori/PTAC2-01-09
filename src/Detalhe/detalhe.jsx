2e3a6555c0c28cbe8e668c2b48085c69f4a4f084
LizFujimori <liizfdaan@gmail.com>
2023-11-24 10:26:04 -0400

    e eu ainda gosto delaaaaaa
    detalhe e componente?

---
+++ src/Detalhe/detalhe.jsx
@@ -0,0 +1,19 @@
+import { useParams } from "react-router-dom";
+import Card from "../../componente/Card";
+
+export default function Detalhe(){
+    const {id} =  useParams();
+    const lista = JSON.parse( localStorage.getItem("lista"));
+    const atividade = lista.filter( (ativ) => {
+
+        if(ativ.id == id){
+            return ativ;
+        }
+        return null;
+    })
+console.log(lista);
+
+return(
+    <Card atividade={atividade[0]}/>
+)
+}
